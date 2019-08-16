479eeb2f812ee4b0e05ed3e32314b6990189e8ae
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-08-16 10:39:03 -0500

    installed npm and axios

---
+++ starwars/src/components/CharacterCard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const CharacterCard = props => {
+    
+    return (
+
+        <div>
+            <div> </div>
+            <div> </div>
+            <div> </div>
+        </div>
+
+    );
+};
+
+export default CharacterCard;
